9905892afa72b857e2a524a4cc56fb887bc02961
gurayyalmaz <guray.yl93@gmail.com>
2025-02-04 09:17:58 +0100

    Initial Commit

---
+++ Die_Worter/Die_WorterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Die_WorterApp.swift
+//  Die_Worter
+//
+//  Created by Gyuray Yalmaz on 04.02.25.
+//
+
+import SwiftUI
+
+@main
+struct Die_WorterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
